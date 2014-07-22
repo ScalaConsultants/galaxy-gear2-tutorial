65d13a32b6ee9f179418ac3dca907167ef001791
Adam Nadoba <anadoba@windowslive.com>
2014-07-22 15:15:18 +0200

    initial commit

---
+++ HelloAccessoryProvider1/app/src/main/java/com/samsung/android/example/helloaccessoryprovider/MyActivity.java
@@ -0,0 +1,43 @@
+package com.samsung.android.example.helloaccessoryprovider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendDataToGear(View view) {
+        Intent intent = new Intent("myData");
+        intent.putExtra("data", "Hello Gear!");
+        sendBroadcast(intent);
+    }
+}
